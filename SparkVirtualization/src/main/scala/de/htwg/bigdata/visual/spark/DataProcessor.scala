bd9b9b38455b0e939988aafe27444ba434ef253e
PatrickK <patrick.krumm@htwg-konstanz.de>
2017-05-14 12:57:16 +0200

    modified ratio calc, added comments

--- SparkVirtualization/src/main/scala/de/htwg/bigdata/visual/spark/DataProcessor.scala
+++ SparkVirtualization/src/main/scala/de/htwg/bigdata/visual/spark/DataProcessor.scala
@@ -27,30 +27,41 @@ class DataProcessor extends java.io.Serializable {
 
   def transformGrid(gridRequest: GridRequest): String = {
 
-    val rdd = loadRDD(gridRequest)
+    // val rdd = loadRDD(gridRequest)
 
+    val sc = new SparkContext("local[*]", "DataProcessor")
+    val config = ConfigFactory.load()
+    val readConfig = new ReadConfig(config.getString("mongodb.db"), gridRequest.collection, Some(config.getString("mongodb.uri")))
+    val rdd = MongoSpark.load(sc, readConfig)
+    println("loaded RDD from MongoDB")
     val columns = extractConfig(rdd)
     val antsPos = extractAntPos(rdd)
-
+    println("extracted Columns and Ant positions")
     //(3,123)
     //(3,222)  <---
     val antNumber = antsPos.map(doc => (doc.getString("id").toInt, doc.getLong("timestamp").toInt))
       .map(item => item.swap).sortByKey(false, 1)
       .map(item => item.swap)
       .reduceByKey((a: Int, b: Int) => a).count()
+      
+    println("Calculated Ant Count")  
 
     val ratio = columns.first.getInteger("rows").toFloat / gridRequest.x.toFloat
+    
+    println("Ratio") 
 
     val firstTimestamp = antsPos.min()(TimeOrdering).getLong("timestamp")
     val lastTimestamp = antsPos.max()(TimeOrdering).getLong("timestamp")
     var currentMillis = 0
+    
+    println("Calculated First and Last Timestamp") 
 
     var gridRepresentation = List[GridRepresentation]()
     var stepCount = 1
     do {
       //current Pos for each timestep
       val currentPos = antsPos.filter(doc => filterCurrentPos(doc, currentMillis))
-
+      println("STEP"+stepCount+" Calculated Ant Count") 
       // sort by timestamp, reduce by id
       //(3,123)
       //(3,222)  <---
@@ -59,22 +70,27 @@ class DataProcessor extends java.io.Serializable {
         .sortByKey(false, 1)
         .map(t => ((t._2.getString("id").toInt, t._2)))
         .reduceByKey((a: Document, b: Document) => a)
+        
+      println("STEP"+stepCount+" Calculated current Ants") 
 
-      currentAnts.foreach(doc => println("current Ants:" + doc))
+
+      //currentAnts.foreach(doc => println("current Ants:" + doc))
 
       //calculate new x and y
       // newX=x*ratio
       val transormedGrid = currentAnts.map(t =>
         {
           var doc = t._2
-          doc.append("newX", (doc.getInteger("x") * ratio).toInt)
-          doc.append("newY", (doc.getInteger("y") * ratio).toInt)
+          doc.append("newX", (Math.max((doc.getInteger("x") / ratio),gridRequest.x-1)).toInt)
+          doc.append("newY", (Math.max((doc.getInteger("y") / ratio),gridRequest.x-1)).toInt)
           doc.append("posID", (doc.get("newX").toString() + "_" + doc.get("newY").toString()))
         }).map(doc => (doc.get("posID").toString().hashCode(), doc))
         .reduceByKey((doc: Document, doc2: Document) => doc)
+              println("STEP"+stepCount+" Calculated transformed Grid") 
 
       //(id,count)
       val antCount = transormedGrid.countByKey()
+            println("STEP"+stepCount+" Calculated Ant Count") 
 
       //calculate concentration
       val newGrid = transormedGrid.map(doc => {
@@ -85,26 +101,30 @@ class DataProcessor extends java.io.Serializable {
         var conc = doc._2.getLong("ants").toFloat / antNumber * 100
         doc._2.append("concentration", conc)
       })
+      
+            println("STEP"+stepCount+" Calculated concentration") 
+
 
       //build json
       val step = GridRepresentation(stepCount, currentMillis, newGrid.collect())
       gridRepresentation ::= step
+            println("STEP"+stepCount+" added in List") 
 
       currentMillis += gridRequest.timestep
       stepCount = stepCount + 1
     } while (currentMillis <= lastTimestamp)
+      println("Finished Calculation, starting tp parse....") 
 
     return parseToJson(gridRepresentation)
   }
 
   private def loadRDD(gridRequest: GridRequest): MongoRDD[Document] = {
-      // Create a SparkContext using every core of the local machine
-      val sc = new SparkContext("local[*]", "DataProcessor")
-      val config = ConfigFactory.load()
-      val readConfig = new ReadConfig(config.getString("mongodb.db"), gridRequest.collection, Some(config.getString("mongodb.uri")))
-      return MongoSpark.load(sc, readConfig)
+    // Create a SparkContext using every core of the local machine
+    val sc = new SparkContext("local[*]", "DataProcessor")
+    val config = ConfigFactory.load()
+    val readConfig = new ReadConfig(config.getString("mongodb.db"), gridRequest.collection, Some(config.getString("mongodb.uri")))
+    return MongoSpark.load(sc, readConfig)
   }
-
 
   private def parseToJson(gridRep: List[GridRepresentation]): String = {
 
@@ -114,6 +134,7 @@ class DataProcessor extends java.io.Serializable {
     mapper.writeValue(out, gridRep)
 
     val json = out.toString()
+    println("finished parsing")
     return json
   }
 
